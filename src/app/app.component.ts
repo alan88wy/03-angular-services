f2d18266156addcf1c9c28b7152390f1b8798964
Alan <alan88w@yahoo.com>
2023-03-28 14:58:11 +0800

    Completed CoursesService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { CoursesService } from './services/courses.service';
 
 @Component({
   selector: 'app-root',
@@ -18,15 +19,16 @@ export class AppComponent implements OnInit {
 
   // courses = {};
 
-  constructor(private http: HttpClient) {
+  // constructor(private http: HttpClient, private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService) {
 
   }
 
   ngOnInit() {
 
-    const params = new HttpParams()
-                    .set("page", 1)
-                    .set("pageSize", 10)
+    // const params = new HttpParams()
+    //                 .set("page", 1)
+    //                 .set("pageSize", 10)
 
 
     // this.http.get('/api/courses', { params })
@@ -38,10 +40,18 @@ export class AppComponent implements OnInit {
 
     // Using Observable. Implicitly subscribe to the http get
     // Making it easy for us to build component that is more reactive
-    this.courses$ = this.http.get<Course[]>('/api/courses', { params });
+    // this.courses$ = this.http.get<Course[]>('/api/courses', { params });
 
+    // We use CourseService instead of doing the above
+    this.courses$ = this.coursesService.loadCourses();
   }
 
 
+  save(course: Course) {
+    this.coursesService.saveCourse(course)
+    .subscribe (
+      () => console.log("Course Saved !")
+    );
+  }
 
 }
