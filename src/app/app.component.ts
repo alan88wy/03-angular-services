7b85d8e9cd7c016d3c2a56bab89951730f4f2271
Alan <alan88w@yahoo.com>
2023-03-28 13:41:39 +0800

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {COURSES} from '../db-data';
+import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
+import {HighlightedDirective} from './directives/highlighted.directive';
+import {Observable} from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  // Can also use observerable
+  courses$ : Observable<Course[]>;
+
+  // courses = {};
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+
+    const params = new HttpParams()
+                    .set("page", 1)
+                    .set("pageSize", 10)
+
+
+    // this.http.get('/api/courses', { params })
+    //         .subscribe(
+    //           res => {
+    //             this.courses = res
+    //           }
+    //         );
+
+    // Using Observable. Implicitly subscribe to the http get
+    // Making it easy for us to build component that is more reactive
+    this.courses$ = this.http.get<Course[]>('/api/courses', { params });
+
+  }
+
+
+
+}
