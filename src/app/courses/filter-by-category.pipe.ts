ee46853dc88ac318a31bea943f4faf9fac33dd78
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-04-06 21:49:47 +0800

    Adjust comment formatting

--- src/app/courses/filter-by-category.pipe.ts
+++ src/app/courses/filter-by-category.pipe.ts
@@ -5,7 +5,8 @@ import { Course } from "../model/course";
 
 @Pipe({
     name: 'filterByCategory',
-    pure: false, // Change pure pipe to non-pure pipe to ensure this will be call whenever the view data is changed.
+    pure: false, // Change pure pipe to non-pure pipe to ensure this will be call whenever
+                 // the view data is changed.
                  // Be aware that this is potentially an expensive operation. So, use with care.
 })
 export class FilterByCategoryPipe implements PipeTransform  {
